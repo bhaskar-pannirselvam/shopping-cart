4af3c5f29a609d990ee3da948a5a8b93c21da4b8
bhaskar-pannirselvam <bhaskar.panirselvam@gmail.com>
2022-09-15 09:27:19 +0530

    Added test cases

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import App from './App';
 import Products  from './pages/Products';
 import Item  from './component/Item';
 import Checkout from './pages/Checkout';
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -15,7 +15,7 @@ import store from './redux/store';
 
 
 
-describe('Rendering default landing page ', () => {
+describe('Tests for Products page ', () => {
   test ('Test if Product page', () => {
   render(
     <Provider store={store}>
@@ -47,7 +47,41 @@ describe('Rendering default landing page ', () => {
       expect(screen.queryByTestId(`cartQty`))
       .toHaveTextContent(/0/);
 
+     
+
     })
+
+    const addBtn = screen.queryByTestId(`add1`);
+    fireEvent.click(addBtn);
+    expect(screen.queryByTestId(`qty1`)).toHaveTextContent(/1/);
+    expect(screen.queryByTestId(`cartQty`))
+      .toHaveTextContent(/0/);
+
+      fireEvent.click(addBtn);
+      expect(screen.queryByTestId(`qty1`)).toHaveTextContent(/2/);
+    expect(screen.queryByTestId(`cartQty`))
+      .toHaveTextContent(/0/);
+
+      fireEvent.click(addBtn);
+      expect(screen.queryByTestId(`qty1`)).toHaveTextContent(/3/);
+    expect(screen.queryByTestId(`cartQty`))
+      .toHaveTextContent(/3/);
+
+      fireEvent.click(addBtn);
+      expect(screen.queryByTestId(`qty1`)).toHaveTextContent(/4/);
+    expect(screen.queryByTestId(`cartQty`))
+      .toHaveTextContent(/4/);
+
+      const remBtn = screen.queryByTestId(`rem1`);
+      fireEvent.click(remBtn);
+      expect(screen.queryByTestId(`qty1`)).toHaveTextContent(/3/);
+      expect(screen.queryByTestId(`cartQty`))
+        .toHaveTextContent(/3/);
+
+        fireEvent.click(remBtn);
+        expect(screen.queryByTestId(`qty1`)).toHaveTextContent(/0/);
+        expect(screen.queryByTestId(`cartQty`))
+          .toHaveTextContent(/0/);
 
 });
 
@@ -174,4 +208,4 @@ const state = {"cart":[
   }
 ]};
 
-const dummyItems = [{ "id": 1, "imageName": "black.jpeg", "name": "Black", "price": 250, "currency": "INR", "minQuantity": 3 }, { "id": 2, "imageName": "blue.jpeg", "name": "Blue", "price": 350, "currency": "INR", "minQuantity": 3 }, { "id": 3, "imageName": "brown.jpeg", "name": "Brown", "price": 350, "currency": "INR", "minQuantity": 2 }, { "id": 4, "imageName": "green.jpeg", "name": "Green", "price": 500, "currency": "INR", "minQuantity": 2 }, { "id": 5, "imageName": "orange.jpeg", "name": "Orange", "price": 250, "currency": "INR", "minQuantity": 1 }, { "id": 6, "imageName": "yellow.jpeg", "name": "Yellow", "price": 350, "currency": "INR", "minQuantity": 1 }];
+const dummyItems = [{ "id": 1, "imageName": "black.jpeg", "name": "Black", "price": 250, "currency": "INR", "minQuantity": 3 }, { "id": 2, "imageName": "blue.jpeg", "name": "Blue", "price": 350, "currency": "INR", "minQuantity": 3 }, { "id": 3, "imageName": "brown.jpeg", "name": "Brown", "price": 350, "currency": "INR", "minQuantity": 2 }, { "id": 4, "imageName": "green.jpeg", "name": "Green", "price": 500, "currency": "INR", "minQuantity": 2 }, { "id": 5, "imageName": "orange.jpeg", "name": "Orange", "price": 250, "currency": "INR", "minQuantity": 1 }, { "id": 6, "imageName": "yellow.jpeg", "name": "Yellow", "price": 350, "currency": "INR", "minQuantity": 1 }]; 
